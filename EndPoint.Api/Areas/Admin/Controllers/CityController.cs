0485dec66f6c186a6f245b245fdfe776d60c5c76
farugh morid ahmadi <ttjam@outlook.com>
2022-10-17 15:18:31 +0330

    کار روی مدیریت شهر

---
+++ EndPoint.Api/Areas/Admin/Controllers/CityController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EndPoint.Api.Areas.Admin.Controllers
+{
+    public class CityController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

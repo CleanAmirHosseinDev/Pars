ce81a7192340644c43b0554dd216470287d49aaa
farugh morid ahmadi <ttjam@outlook.com>
2022-10-20 11:09:32 +0330

    تغیرات برای محیط مشتری

--- ParsKyanCrm.Application/Dtos/Users/CustomersDto.cs
+++ ParsKyanCrm.Application/Dtos/Users/CustomersDto.cs
@@ -13,45 +13,81 @@ namespace ParsKyanCrm.Application.Dtos.Users
         public int CustomerID { get; set; }
         
         public int? CityID { get; set; }
-        
+        /// <summary>
+        /// نام شرکت
+        /// </summary>
         public string CompanyName { get; set; }
-
+        /// <summary>
+        /// اسامی امضاکنندگان مجاز
+        /// </summary>
         public string NamesAuthorizedSignatories { get; set; }
-        
+        /// <summary>
+        /// نوع شرکت
+        /// </summary>
         public int? KindOfCompanyID { get; set; }
-
+        /// <summary>
+        /// 
+        /// </summary>
         public int? TypeServiceRequestedID { get; set; }
-
+        /// <summary>
+        /// چگونه شناسه شرکت را بشناسیم
+        /// </summary>
         public int? HowGetKnowCompanyID { get; set; }
-
+        /// <summary>
+        /// آدرس شرکت
+        /// </summary>
         public string AddressCompany { get; set; }
-
+        /// <summary>
+        /// شناسه ملی  شرکت
+        /// </summary>
         public string NationalCode { get; set; }
-       
+        /// <summary>
+        /// کد پستی
+        /// </summary>
         public string PostalCode { get; set; }
-
+        /// <summary>
+        /// کد اقتصادی
+        /// </summary>
         public string EconomicCode { get; set; }
-
+        /// <summary>
+        /// تلفن شرکت
+        /// </summary>
         public string Tel { get; set; }
-
+        /// <summary>
+        /// ایمیل
+        /// </summary>
         public string Email { get; set; }
-
+        /// <summary>
+        /// نام رابط و نماینده شرکت
+        /// </summary>
         public string AgentName { get; set; }
-
+        /// <summary>
+        /// موبایل نماینده
+        /// </summary>
         public string AgentMobile { get; set; }
-        
+        /// <summary>
+        /// نام و نام خانوادگی مدیر عامل
+        /// </summary>
         public string CeoName { get; set; }
-
+        /// <summary>
+        /// موبایل مدیر عامل
+        /// </summary>
         public string CeoMobile { get; set; }
-
+        /// <summary>
+        /// تعداد پرسنل شرکت
+        /// </summary>
         public int? CountOfPersonal { get; set; }
-
+        /// <summary>
+        /// مبلغ کل فروش اظهار شده
+        /// </summary>
         public decimal? AmountOsLastSaels { get; set; }
 
         public DateTime SaveDate { get; set; }
 
         public string Ip { get; set; }
-        
+        /// <summary>
+        /// کد احراز هویت
+        /// </summary>
         public string AuthenticateCode { get; set; }
 
         public CityDto City { get; set; }

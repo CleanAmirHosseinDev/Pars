aa408ce40dff3caab054cb41e054b439204f8525
monirekhanlou <75141554+monirekhanlou@users.noreply.github.com>
2022-12-08 09:38:06 +0330

    edit

--- EndPoint/wwwroot/js/Views/Admin/ManagerOfParsKyan.js
+++ EndPoint/wwwroot/js/Views/Admin/ManagerOfParsKyan.js
@@ -24,7 +24,7 @@
                 var strM = '';
                 for (var i = 0; i < res.data.length; i++) {
 
-                    strM += "<tr><td>" + (i + 1) + "</td><td>" + res.data[i].nameOfManager + "</td><td>" + res.data[i].title.label + "</td><td>" + res.data[i].position.label + "</td><td><a title='ویرایش' href='/Admin/ManagerOfParsKyan/EditManagerOfParsKyan?id=" + res.data[i].ManagerOfParsKyanId + "' class='btn btn-edit fontForAllPage'><i class='fa fa-edit'></i></a></td></tr>";
+                    strM += "<tr><td>" + (i + 1) + "</td><td>" + res.data[i].nameOfManager + "</td><td>" + res.data[i].title.label + "</td><td>" + res.data[i].position.label + "</td><td><a title='ویرایش' href='/Admin/ManagerOfParsKyan/EditManagerOfParsKyan?id=" + res.data[i].managersId + "' class='btn btn-edit fontForAllPage'><i class='fa fa-edit'></i></a></td></tr>";
 
                 }
                 $("#tBodyList").html(strM);

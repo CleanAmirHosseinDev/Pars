00f1d78075cd693d39d22daaa0de275a13abd694
monirekhanlou <75141554+monirekhanlou@users.noreply.github.com>
2022-12-19 17:31:55 +0330

    نمایش مشخصات مشتری در قسمت ارجاع ناحیه سوپروایزر

---
+++ EndPoint/Controllers/api/superVisor/ContractController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ParsKyanCrm.Application.Dtos.BasicInfo;
+using ParsKyanCrm.Application.Dtos.Users;
+using ParsKyanCrm.Application.Patterns.FacadPattern;
+using ParsKyanCrm.Common.Dto;
+using ParsKyanCrm.Common.Enums;
+using ParsKyanCrm.Infrastructure.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EndPoint.Controllers.api.superVisor
+{
+   
+    public class ContractController : BaseController
+    {
+        private readonly ILogger<ContractController> _logger;
+        private readonly IUserFacad _userFacad;
+        public ContractController(ILogger<ContractController> logger, IUserFacad userFacad)
+        {
+            _logger = logger;
+            _userFacad = userFacad;
+        }
+
+
+        [Route("[action]/{id}/")]
+        [HttpGet]
+      //  [UserRoleAdminRolesFilter(Role = new[] { UserRoleAdminRoles.Contract_Save })]
+        public async Task<ContractDto> Get_Contract(int? id = null)
+        {
+            try
+            {
+                return await _userFacad.GetContractService.Execute(new RequestContractDto() {ContractId = id });
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        
+
+    }
+}
---
+++ EndPoint/wwwroot/js/Views/SuperVisor/Customer.js
@@ -0,0 +1,139 @@
+﻿
+
+
+
+
+
+
+(function (web, $) {
+
+    //Document Ready              
+
+    function textSearchOnKeyDown(event) {
+
+        if (event.keyCode == 13) $(`button[title='جستجو']`).click();
+
+    }
+
+    function saveCustomer(e) {
+
+        $(e).attr("disabled", "");
+
+        AjaxCallAction("POST", "/api/customer/Customers/Save_BasicInformationCustomers", JSON.stringify({
+
+            tel: $("#Tel").val(),
+            AddressCompany: $("#AddressCompany").val(),
+            CompanyName: $("#CompanyName").val(),
+            CeoName: $("#CeoName").val(),
+            EconomicCode: $("#EconomicCode").val(),
+            NationalCode: $("#NationalCode").val(),
+            CeoMobile: $("#CeoMobile").val(),
+            CeoNationalCode: $("#CeoNationalCode").val(),
+            AgentMobile: $("#AgentMobile").val(),
+            AgentName: $("#AgentName").val(),
+            NamesAuthorizedSignatories: $("#NamesAuthorizedSignatories").val(),
+            AmountOsLastSaels: isEmpty($("#AmountOsLastSaels").val()) ? null : $("#AmountOsLastSaels").val(),
+            CountOfPersonal: isEmpty($("#CountOfPersonal").val()) ? null : $("#CountOfPersonal").val(),
+            Email: $("#Email").val(),
+            Tel: $("#Tel").val(),
+            PostalCode: $("#PostalCode").val(),
+            HowGetKnowCompanyId: isEmpty($("#howGetKnowCompany").val()) ? null : $("#howGetKnowCompany").val(),
+            KindOfCompanyId: isEmpty($("#KindOfCompany").val()) ? null : $("#KindOfCompany").val(),
+            LastInsuranceList: null,
+            LastChangeOfficialNewspaper: null,
+            
+        }), true, function (res) {
+
+            $(e).removeAttr("disabled");
+
+            if (res.isSuccess) {
+
+                if ($("#TypeServiceRequestedId").val() != null) {
+                    saveFirstRequestForRating($("#TypeServiceRequestedId").val());
+                }
+
+                alertB("ثبت", res.message, "success");
+                $("SeeAllRequest").show();
+                //  goToUrl("/Customer/Customer/EditCustomer");
+
+            } else {
+
+                alertB("خطا", res.message, "error");
+            }
+
+        }, true);
+
+    }
+
+    function saveFirstRequestForRating(id) {
+       
+        AjaxCallAction("POST", "/api/customer/RequestForRating/Save_Request", JSON.stringify({ Request: { KindOfRequest: isEmpty(id) ? null :id } }), true, function (res) {
+
+            if (!res.isSuccess) {
+                alertB("هشدار", res.message, "warning");
+            }
+
+        }, true);
+
+    }
+
+
+    function systemSeting_Combo(resSingle) {
+
+        AjaxCallAction("POST", "/api/customer/SystemSeting/Get_SystemSetings", JSON.stringify({ ParentCodeArr: "63,27,56", PageIndex: 0, PageSize: 0 }), true, function (res) {
+
+            if (res.isSuccess) {
+                var strKindOfCompany = '<option value="">انتخاب کنید</option>';
+                var strHowGetKnowCompany = '<option value="">انتخاب کنید</option>';
+                var strTypeServiceRequestedId = '<option value="">انتخاب کنید</option>';
+
+                for (var i = 0; i < res.data.length; i++) {
+                    if (res.data[i].parentCode == 56) {
+                        strHowGetKnowCompany += " <option value=" + res.data[i].systemSetingId + ">" + res.data[i].label + "</option>";
+                    } else if (res.data[i].parentCode == 63) {
+                        strTypeServiceRequestedId += " <option value=" + res.data[i].systemSetingId + ">" + res.data[i].label + "</option>";
+                    } else if (res.data[i].parentCode == 27) {
+                        strKindOfCompany += " <option value=" + res.data[i].systemSetingId + ">" + res.data[i].label + "</option>";
+                    }
+                }
+
+                $("#KindOfCompany").html(strKindOfCompany);
+                $("#HowGetKnowCompany").html(strHowGetKnowCompany);
+                $("#TypeServiceRequestedId").html(strTypeServiceRequestedId);
+
+                $("#HowGetKnowCompany").val(resSingle.howGetKnowCompanyId);
+                $("#KindOfCompany").val(resSingle.kindOfCompanyId);
+                $("#TypeServiceRequestedId").val(resSingle.typeServiceRequestedId);              
+
+            }
+        }, true);
+    }
+
+    function checkForFirstRequest() {
+        Web.RequestForRating.RegistingFirstRequest(function (res) {
+
+            if (res.isSuccess) {
+                if (res.data.length > 0) {
+                    $("#divTypeServiceRequestedId").hide();
+                }
+
+                else {
+                    $("#divTypeServiceRequestedId").show();
+                }
+            } 
+
+        });
+    }
+
+   
+
+    web.Customer = {
+        TextSearchOnKeyDown: textSearchOnKeyDown,
+        SaveCustomer: saveCustomer,
+        InitCustomer: initCustomer,
+        SystemSeting_Combo: systemSeting_Combo,
+        CheckForFirstRequest: checkForFirstRequest,
+        SaveFirstRequestForRating: saveFirstRequestForRating
+    };
+
+})(Web, jQuery);

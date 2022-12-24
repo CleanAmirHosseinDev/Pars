12e3839970a339f7ee84e53fd7cda74e8e22f490
Mohammad Javdan <mohammadjv7@gmail.com>
2022-12-24 14:14:52 +0330

    dynamic activity

--- EndPoint/Controllers/ActivityController.cs
+++ EndPoint/Controllers/ActivityController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ParsKyanCrm.Application.Dtos.BasicInfo;
+using ParsKyanCrm.Application.Patterns.FacadPattern;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,14 +14,22 @@ namespace EndPoint.Controllers
     {
 
         private readonly ILogger<ActivityController> _logger;
-        public ActivityController(ILogger<ActivityController> logger)
+        private readonly IBasicInfoFacad _basicInfoFacad;
+        public ActivityController(ILogger<ActivityController> logger,IBasicInfoFacad basicInfoFacad)
         {
             _logger = logger;
+            _basicInfoFacad = basicInfoFacad;
         }
 
-        public IActionResult Index(int? id = null)
+        public async Task<IActionResult> Index(int? id = null)
         {
             ViewBag.id = id;
+            try {
+                var data = await _basicInfoFacad.GetActivityService.Execute(new RequestActivityDto() { ActivityId = id });
+                ViewData["data"] = data;
+            } catch(Exception) {
+                throw;
+            }
             return View();
         }
 

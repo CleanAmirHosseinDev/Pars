7088474cb5b6a139d95a85414335816360b71844
Mohammad Javdan <mohammadjv7@gmail.com>
2022-12-26 11:16:12 +0330

    ContactUs Form is ok

--- EndPoint/Controllers/AboutUsController.cs
+++ EndPoint/Controllers/AboutUsController.cs
@@ -1,40 +1,75 @@
 ﻿using EndPoint.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.Extensions.Logging;
+using ParsKyanCrm.Application.Dtos.BasicInfo;
+using ParsKyanCrm.Application.Services.Email;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using static ParsKyanCrm.Application.Services.WebService.CaptchaService;
 
-namespace EndPoint.Controllers
-{
-    public class AboutUsController : Controller
-    {
+namespace EndPoint.Controllers {
+    public class AboutUsController :Controller {
         private readonly ILogger<AboutUsController> _logger;
 
-        public AboutUsController(ILogger<AboutUsController> logger)
-        {
+        public AboutUsController(ILogger<AboutUsController> logger) {
             _logger = logger;
         }
 
-      
-        public IActionResult Index()
-        {
+
+        public IActionResult Index( ) {
             return View();
         }
-        public IActionResult VisionAndMission()
-        {
+        public IActionResult VisionAndMission( ) {
             return View();
         }
-        public IActionResult OrganazationChart()
-        {
+        public IActionResult OrganazationChart( ) {
             return View();
         }
 
-        public IActionResult ContactUs()
-        {
+        public IActionResult ContactUs( ) {
             return View();
+        }
+        [CaptchaCheck]
+        [HttpPost]
+        public JsonResult ContactUsForm([FromForm]ContactUsFormDto form) {
+            var error = "";
+            Boolean isOk = false;
+            if(ModelState.IsValid) {
+                try {
+                    if(form.email == null || form.email.Length == 0) form.email = "nomail@z.z";
+                    if(form.subject == null || form.subject.Length == 0) form.subject = "بدون موضوع";
+                    string prependstr =
+                    "<br>موضوع: " + form.subject
+                    + "<br>نام: " + form.name
+                    + "<br>ایمیل: " + form.email
+                    + "<br>تلفن: " + form.phone
+                    + "<br>-----------------------------------<br>";
+
+                    form.message = prependstr + form.message;
+
+                    EmailSend.SendMail(form.email, form.name, form.subject, form.message, "info@parscrc.ir");
+                    isOk = true;
+                } catch(Exception ex) {
+                    error = "خطا در اجرای درخواست";
+                }
+            } else {
+                var errors = ModelState.Select(x => x.Value.Errors)
+                           .Where(y => y.Count > 0)
+                           .ToList();
+                foreach(var item in errors) {
+                    error += item.First().ErrorMessage + " ";
+                }
+
+            }
+            return Json(new {
+                IsSuccess = isOk,
+                Message = isOk?"پیام شما با موفقیت ارسال شد.": error
+            });
+
         }
     }
 }

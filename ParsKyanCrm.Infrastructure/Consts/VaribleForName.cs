673e8ad405826f8bbca52e01eebdc7beedfde285
monirekhanlou <75141554+monirekhanlou@users.noreply.github.com>
2023-02-28 16:15:03 +0330

    edit

--- ParsKyanCrm.Infrastructure/Consts/VaribleForName.cs
+++ ParsKyanCrm.Infrastructure/Consts/VaribleForName.cs
@@ -10,9 +10,9 @@ namespace ParsKyanCrm.Infrastructure.Consts
     {
 
         /// <summary>
-        /// true ==> Debug
-        /// false ==> Server Main
-        /// null ==> Server Test
+        /// true ==> Debug for  developer
+        /// false ==> Server Main 
+        /// null ==> Server Test for dev
         /// </summary>
         public static bool? IsDebug
         {
@@ -29,7 +29,7 @@ namespace ParsKyanCrm.Infrastructure.Consts
                 if (IsDebug == null)
                     return "data source=172.16.21.6;initial catalog=TestParsKyanCrmDB;user id=pars;password=Pars@10155;MultipleActiveResultSets=True";
                 else if (IsDebug == true)
-                    return "data source=77.238.123.197;initial catalog=ParsKyanCrmDB;user id=pars;password=Pars@10155;MultipleActiveResultSets=True";
+                    return "data source=172.16.21.6;initial catalog=adasd;user id=pars;password=Pars@10155;MultipleActiveResultSets=True";
                 else
                     return "data source=172.16.21.6;initial catalog=ParsKyanCrmDB;user id=pars;password=Pars@10155;MultipleActiveResultSets=True";
 

e057a539d5782df94b3f4e4241ccd1bcaf2a5449
hamid Sarfarazi <sarfarazi@hotmail.com>
2023-02-28 14:13:56 +0330

    Change Connection Srting

--- ParsKyanCrm.Infrastructure/Consts/VaribleForName.cs
+++ ParsKyanCrm.Infrastructure/Consts/VaribleForName.cs
@@ -29,7 +29,7 @@ namespace ParsKyanCrm.Infrastructure.Consts
                 if (IsDebug == null)
                     return "data source=172.16.21.6;initial catalog=TestParsKyanCrmDB;user id=pars;password=Pars@10155;MultipleActiveResultSets=True";
                 else if (IsDebug == true)
-                    return "data source=77.238.123.197;initial catalog=ParsKyanCrmDB;user id=pars;password=Pars@10155;MultipleActiveResultSets=True";
+                    return "data source=172.16.21.6;initial catalog=MonirMobinDB;user id=pars;password=Pars@10155;MultipleActiveResultSets=True";
                 else
                     return "data source=172.16.21.6;initial catalog=ParsKyanCrmDB;user id=pars;password=Pars@10155;MultipleActiveResultSets=True";
 

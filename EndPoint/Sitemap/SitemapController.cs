a0f43a7ff3d8ffce2cb277cfa0f7f89babad90d2
HamidReza <hamid10631021@hotmail.com>
2024-03-16 16:16:20 +0330

    تغییرات سئو

---
+++ EndPoint/Sitemap/SitemapController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ParsKyanCrm.Application.Dtos.Users;
+using ParsKyanCrm.Application.Patterns.FacadPattern;
+using System;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace EndPoint.Sitemap
+{
+    public class SitemapController : SitemapList
+    {
+        private IUserFacad _userFacad;
+        public SitemapController(IUserFacad userFacad)
+        {
+            _userFacad = userFacad;
+        }
+        [HttpGet("sitemap.xml")]
+        public async System.Threading.Tasks.Task<IActionResult> IndexAsync()
+        {
+            add_url(Url.Action("Index", "Home"), modified: DateTime.Now, changeFrequency: "daily", priority: "1.0");
+            var content = (await _userFacad.GetNewsAndContentsService.Execute(new RequestNewsAndContentDto()));
+
+            //add_url(Url.Action("Article", "Content", obj.ContentId), (DateTime)obj.DateSave, "monthly", priority: "1.0");
+
+            XNamespace ns = "http://www.sitemaps.org/schemas/sitemap/0.9";
+            var sitemap = new XDocument(new XDeclaration("1.0", "utf-8", null),
+                new XElement(ns + "urlset",
+                    // Add URL entries here
+                    from item in GetSitemapItems()
+                    select CreateUrlElement(item)
+                ));
+
+            return Content(sitemap.ToString(), "text/xml", Encoding.UTF8);
+        }
+    }
+
+}
---
+++ EndPoint/Sitemap/SitemapModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EndPoint.Sitemap
+{
+    public class SitemapModel
+    {
+        public string Url { get; }
+        public DateTime Modified { get; }
+        public string ChangeFrequency { get; }
+        public string Priority { get; }
+
+        public SitemapModel(string url, DateTime? modified, string? changeFrequency, string? priority)
+        {
+            Url = url;
+            Modified = (DateTime)modified;
+            ChangeFrequency = changeFrequency;
+            Priority = priority;
+        }
+        // ChangeFrequency : always - hourly - daily - weekly - monthly - yearly - never
+    }
+}

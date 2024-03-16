048688eec2bfc24357a9bf94b384ee1ee1ca91d4
HamidReza <hamid10631021@hotmail.com>
2024-03-16 13:58:49 +0330

    اضافه کردن سایت مپ

---
+++ EndPoint/Sitemap/SitemapModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EndPoint.Sitemap
+{
+    public class SitemapModel
+    {
+        public string Url { get; }
+        public DateTime Modified { get; }
+        public string ChangeFrequency { get; }
+        public string Priority { get; }
+
+        public SitemapModel(string url, DateTime modified, string changeFrequency, string priority)
+        {
+            Url = url;
+            Modified = modified;
+            ChangeFrequency = changeFrequency;
+            Priority = priority;
+        }
+    }
+}

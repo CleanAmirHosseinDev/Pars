39bc58a2d76b24b6c5ecebcd774a8017d9b75796
HamidReza <hamid10631021@hotmail.com>
2024-06-24 08:55:32 +0330

    افزودن به ناحیه ادمین اپشن های سوالات لیکرتی

---
+++ EndPoint/Areas/Admin/Controllers/CorporateController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ParsKyanCrm.Application.Services.WebService;
+using RestSharp;
+using RestSharp.Authenticators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EndPoint.Areas.Admin.Controllers
+{
+    public class CorporateController : BaseController
+    {
+        public IActionResult DataFormQuestions()
+        {
+            return View();
+        }
+
+        public IActionResult EditDataFormQuestions(int? id = null)
+        {
+            ViewBag.id = id;
+            return View();
+        }
+
+        public IActionResult DataFormQuestionsOptione()
+        {
+            return View();
+        }
+
+        public IActionResult EditDataFormQuestionsOptione()
+        {
+            return View();
+        }
+    }
+}
---
+++ EndPoint/Controllers/api/admin/CorporateController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ParsKyanCrm.Application.Dtos.Users;
+using ParsKyanCrm.Application.Patterns.FacadPattern;
+using ParsKyanCrm.Common.Dto;
+using ParsKyanCrm.Common.Enums;
+using ParsKyanCrm.Infrastructure.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EndPoint.Controllers.api.admin
+{
+    [UserRoleAdminRolesFilter(Role = new[] { UserRoleAdminRoles.City })]
+    public class CorporateController : BaseController
+    {
+        private readonly ILogger<CorporateController> _logger;
+        private readonly IUserFacad _userFacad;
+        public CorporateController(ILogger<CorporateController> logger, IUserFacad userFacad)
+        {
+            _logger = logger;
+            _userFacad = userFacad;
+        }
+
+        [Route("[action]")]
+        [HttpPost]
+        public async Task<ResultDto<IEnumerable<DataFormQuestionsOptionDto>>> Get_DataFormQuestionsOptione([FromBody] RequestDataFormQuestionsOptionDto request)
+        {
+            try
+            {
+                return await _userFacad.GetDataFormQuestionsOptionService.Execute(request);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        [Route("[action]/{id}/")]
+        [HttpGet]
+        [UserRoleAdminRolesFilter(Role = new[] { UserRoleAdminRoles.City_Save })]
+        public async Task<DataFormQuestionsOptionDto> Get_DataFormQuestionsOptione(int? id = null)
+        {
+            try
+            {
+                return; //await _userFacad.GetDataFormQuestionsOptionService.Execute(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        [UserRoleAdminRolesFilter(Role = new[] { UserRoleAdminRoles.City_Save })]
+        public async Task<ResultDto<CityDto>> Save_City([FromBody] CityDto request)
+        {
+            try
+            {
+                return await _userFacad.SaveCityService.Execute(request);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        [HttpPost]
+        [Route("[action]")]        
+        public async Task<ResultDto<IEnumerable<StateDto>>> Get_States_Combo(RequestStateDto request)
+        {
+            try
+            {
+                return (await _userFacad.GetStatesService.Execute(request));
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+    }
+}
